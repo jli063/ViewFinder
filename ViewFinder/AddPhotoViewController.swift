0fc345cfb7eca2d2f743f0c71349ea42f070d8ba
Apple <apple@Apples-MacBook-Pro-15.local>
2019-06-12 09:58:36 -0500

    Complete Iteration 2

--- ViewFinder/AddPhotoViewController.swift
+++ ViewFinder/AddPhotoViewController.swift
@@ -24,24 +24,38 @@ var imagePicker = UIImagePickerController()
             present(imagePicker, animated: true, completion: nil)
     }
     
+    @IBOutlet weak var newImage: UIImageView!
+    @IBOutlet weak var captionText: UITextField!
+    @IBAction func savePhoto(_ sender: UIButton) {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            let photoToSave = Photos(entity: Photos.entity(), insertInto: context)
+            photoToSave.caption = captionText.text
+        if let userImage = newImage.image {
+            if let userImageData = userImage.pngData() {
+                photoToSave.imageData = userImageData
+            }
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+        navigationController?.popViewController(animated: true)
+    }
     
-    @IBAction func chooseLibrary(_ sender: Any) {
+    
+        func chooseLibrary(_ sender: Any) {
             imagePicker.sourceType = .photoLibrary
             present(imagePicker, animated: true, completion: nil)
     }
     
-    @IBAction func chooseAlbum(_ sender: Any) {
+        func chooseAlbum(_ sender: Any) {
         imagePicker.sourceType = .savedPhotosAlbum
         present(imagePicker, animated: true, completion: nil)
     }
     
-    @IBOutlet weak var newImage: UIImageView!
 
-    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
     
     if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
         newImage.image = selectedImage
         }
     imagePicker.dismiss(animated:true, completion:nil)
     }
+}
 }
